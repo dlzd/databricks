729d8d19c4fe94b03b2f53245ea43f5bab419ae5
erictome-db <eric.tome@databricks.com>
2022-08-28 16:01:54 -0500

    indent fix

--- blogs/cdf_delta_sharing/data_generator/incremental_data_load.py
+++ blogs/cdf_delta_sharing/data_generator/incremental_data_load.py
@@ -23,7 +23,7 @@ from delta.tables import *
 # DBTITLE 1,Generate Update Data
 cdc_inc_data_spec = (dg.DataGenerator(spark, rows=100000, partitions = 1)
     .withColumn('RECID', 'int' , uniqueValues=1000000)
-	.withColumn('COMPANYNAME', 'string' , values=['Company1','Company2','Company3'])
+	  .withColumn('COMPANYNAME', 'string' , values=['Company1','Company2','Company3'])
     .withColumn('QUANTITY', 'int' , minValue=5, maxValue=10, random=True)
     .withColumn("UPDATE_TIME", "timestamp", expr="current_timestamp()"))
 
