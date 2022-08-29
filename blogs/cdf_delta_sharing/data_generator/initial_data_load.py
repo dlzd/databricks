5b1da1f71ae2b79ca09d7eaa2f1fd53d33045557
dlzd <erictome@gmail.com>
2022-08-29 11:25:56 -0500

    Update initial_data_load.py

--- blogs/cdf_delta_sharing/data_generator/initial_data_load.py
+++ blogs/cdf_delta_sharing/data_generator/initial_data_load.py
@@ -23,7 +23,7 @@ from dbldatagen import fakerText
 
 cdc_data_spec = (dg.DataGenerator(spark, rows=1000000, partitions = 10)
     .withColumn('RECID', 'int' , uniqueValues=1000000)
-	.withColumn('COMPANYNAME', 'string' , values=['Company1','Company2','Company3'])
+    .withColumn('COMPANYNAME', 'string' , values=['Company1','Company2','Company3'])
     .withColumn('QUANTITY', 'int' , minValue=1, maxValue=5, random=True)
     .withColumn("UPDATE_TIME", "timestamp", expr="current_timestamp()"))
 
